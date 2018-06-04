0c0365a403ba245c35e9756c382f1b79efcbdafb
Jonathan Smith <jonathan223@gmail.com>
2018-06-04 02:22:38 -0500

    attribute-based routing

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,12 +13,8 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "MoviesByReleaseDate",
-                url: "movies/released/{year}/{month}",
-                defaults: new { controller = "Movies", action = "ByReleaseDate" },
-                constraints: new {year = @"\d{4}", month = @"\d{2}"}
-            );
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
